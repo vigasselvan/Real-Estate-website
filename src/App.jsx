87d1b3308607970b9e50913119c98228eb6ab196
vigas <vigas54209@gmail.com>
2023-12-08 07:09:40 +0530

    added header for the website.

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,12 @@ import Home from "./pages/Home"
 import Profile from "./pages/Profile"
 import SignIn from "./pages/Signin"
 import SignUp from "./pages/SignUp"
+import Header from "./components/Header"
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home/>} />
         <Route path="/about" element={<About/>} />
